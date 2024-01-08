893a7cecba2deaad0a1c0a3bea7bd56badfb5688
asbtho-eq <asth@equinor.com>
2024-01-08 16:55:57 +0100

    day 16 update

--- 16/16.py
+++ 16/16.py
@@ -3,6 +3,8 @@ def main():
 
     emptyMap = createEmptyMap(map)
 
+    queue = [(0,0,"right")]
+
     for line in emptyMap:
         print(line)
 
@@ -15,8 +17,44 @@ def createEmptyMap(map):
     return newMap
 
 
-def checkTile(map, this, next):
-    print("START")
+def checkTile(map, direction, this):
+    dirC = {
+        "right": (0,1),
+        "down": (1,0),
+        "left": (0,-1),
+        "up": (-1,0),
+        "left_right": ((0,-1),(0,1)),
+        "up_down": ((-1,0),(1,0))
+    }
+
+    yC, xC = this[0], this[1]
+    tile = map[yC][xC]
+
+    if tile == ".":
+        return dirC[direction]
+    
+    if tile == "/":
+        if direction == "right":
+            return dirC["up"]
+        elif direction == "down":
+            return dirC["left"]
+        elif direction == "left":
+            return dirC["down"]
+        else:
+            return dirC["right"]
+        
+    if tile == "|":
+        if direction == "down" or direction == "up":
+            return dirC[direction]
+        else:
+            return dirC["left_right"]
+    
+    if tile == "-":
+        if direction == "left" or direction == "right":
+            return dirC[direction]
+        else:
+            return dirC["up_down"]
+
 
 
 main()
